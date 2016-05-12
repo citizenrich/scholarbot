4bb0225af32c4bd517a90979702f7cfd8c4febd3
Richard Stanley <richard@thrillride.local>
2016-05-12 22:42:16 +0100

    remove cruft

--- itfriend.py
+++ itfriend.py
@@ -20,4 +20,4 @@ def hello_world():
     #fyi, textit.in can index arrays and do nested, e.g. @extra.products.0.name
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', debug = False)
