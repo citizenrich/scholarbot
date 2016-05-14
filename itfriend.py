a5a811ff4e2270e34a350df93361f6a7536ebc05
Richard Stanley <richard@thrillride.local>
2016-05-14 06:36:06 +0100

    serialiser

--- itfriend.py
+++ itfriend.py
@@ -3,12 +3,6 @@ from flask import Flask, jsonify, request
 from crossrefquery import *
 import json
 
-#test
-# stuff = 'book' #book, dissertation, monograph, journal-article, book-chapter
-# test = ['Cold War']
-# z = requestarticles(stuff, test)
-# print z
-
 app = Flask(__name__)
 
 @app.route('/v1') #http://127.0.0.1:5000/v1?words=Stalin
@@ -16,9 +10,8 @@ def hello_world():
     cat = (request.args.get('cat'))
     words = [request.args.get('words')]
     output = requestarticles(cat, words)
-    length = len(output)
-    return jsonify({'length': length, 'results': output})
+    return jsonify({'results': output})
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', debug = False)
-    app.run(debug = True)
+    app.run(host='0.0.0.0', debug = False)
+    #app.run(debug = True)
