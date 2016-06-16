99c92ca64099da46314076cc286062a67e4e22cf
Richard Stanley <richard@thrillride.local>
2016-06-16 10:49:17 +0100

    remove old file

--- test.py
+++
@@ -1,5 +0,0 @@
-myList = [{'url': u'http://dx.doi.org/10.4324/9781315733661', 'intitle': [u'Cold War'], 'title': u'Cold War Theatre (Routledge Revivals)'}, {'url': u'http://dx.doi.org/10.4324/9781315780962', 'intitle': [u'Cold War'], 'title': u'Thailand in the Cold War'}]
-
-
-for i, title in enumerate(d for d in myList):
-    print i,d.get('title')
