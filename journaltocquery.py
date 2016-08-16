015fa9a17d252c0ff924a72e47b4986f641a061b
Richard Stanley <richard@thrillride.local>
2016-08-16 22:55:30 +0100

    lower limit for jtoc

--- journaltocquery.py
+++ journaltocquery.py
@@ -6,7 +6,7 @@ class JournalTOC(object):
     def getjournaltoc(self, keywords):
         results = []
         base = 'http://www.journaltocs.ac.uk/api/articles/'
-        limit = 15
+        limit = 10
         params = {'to': limit}
         url = '%s%s' % (base, keywords)
         response = requests.get(url, params=params)
